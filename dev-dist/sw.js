77fef6dfec8f6ae1444384171d71eb38b5ff2d37
Shukan Parmar <shukan.kirit.28feb@gmail.com>
2025-12-08 23:33:17 +0530

    downloaded package

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1h5ib2tlgmo"
+    "revision": "0.f8343o3o3l"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
